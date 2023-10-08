866ce34f8f569c9dd8596c0be78c7b25c307aec4
Antonioplatypus <76115170+Antonioplatypus@users.noreply.github.com>
2023-10-08 23:14:52 +0300

    Add grid size changer

--- myjavascript.js
+++ myjavascript.js
@@ -1,13 +1,27 @@
 var columns = 16;
 var rows = 16;
 var colorUsed = "black";
+var gridSize = 0;
+var gridCreated = 0;
+gridContainer = document.getElementById("gridContainer");
+//document.getElementById("grid")
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function CreateGrid(gridSize) {
+if(gridCreated != 0){ removeAllChildNodes(gridContainer); gridCreated = 0; }
 var grid = document.createElement('div');            // creates grid
 grid.className = 'grid';
-for (var i = 0; i < columns; ++i) {
+if (gridSize>100) gridSize = 64;
+if (gridSize<1) gridsize = 64;
+for (var i = 0; i < gridSize; ++i) {
     var column = document.createElement('div');      // creates column
     column.className = 'column';
-        for (var j = 0; j < rows; ++j) {
+        for (var j = 0; j < gridSize; ++j) {
             var cell = document.createElement('div');    // creates cell
             cell.className = 'cell';
             cell.textContent = "cell";                   // sets text in cell
@@ -15,9 +29,16 @@ for (var i = 0; i < columns; ++i) {
         }
     grid.appendChild(column);                        // appends column to grid
 }
-document.body.appendChild(grid);
+document.getElementById("gridContainer").appendChild(grid); gridCreated = 1;
+}
 
-grid.addEventListener("mouseover", (event) => {
+document.getElementById("gridSize").addEventListener("click", (event) => { 
+    gridSize = window.prompt("How big should the grid be? (100 max)");
+    CreateGrid(gridSize)
+    ;});
+
+
+gridContainer.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = colorUsed;
   });
 
